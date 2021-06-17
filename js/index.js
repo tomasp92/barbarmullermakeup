cb152edd19513b7ae33c48175badc298f88350ce
tomasp92 <t.palau.gcba@gmail.com>
2021-06-17 11:26:19 -0300

    solucionar problemas de merge 3

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
 let divCarrito = document.querySelector('#carrito');
 let divShop = document.querySelector('#shop');
-
 function DisplayCarrito(carrito) {
     if(carrito > 0){
         divCarrito.style.display = 'block';
@@ -11,7 +10,6 @@ function DisplayCarrito(carrito) {
         divShop.style.display = 'block';
     }
 }
-
 function SeparateProductosSercicios(productos){
     let Productos = [];
     let Servicios = [];
@@ -24,55 +22,51 @@ function SeparateProductosSercicios(productos){
     }
     return [Productos,Servicios]
 }
-
-
-function DisplayCarouselProductos(){}
 // carousels con productos y servicios
-function DisplayCarouselServicios(servicios){
-    counterServicios = 0;
-    for (let servicio of servicios){
+function DisplayCarousel(items, itemTipe){
+    counterItem = 0;
+    for (let item of items){
         let carouselItem = document.createElement('div');
         card = ` 
         <div class="carouselCard">  
-            <div onclick="DisplayOneProducto(${servicio.id});"> 
-                <img  src="${servicio.img}" alt="${servicio.titulo}"> 
+            <div onclick="DisplayOneProducto(${item.id});"> 
+                <img  src="${item.img}" alt="${item.titulo}"> 
             </div>
-            <div class="tituloIndex titulo" onclick="DisplayOneProducto(${servicio.id});">${servicio.titulo}</div>
-            <div class="subtituloIndex subtitulo" onclick="DisplayOneProducto(${servicio.id});">${servicio.subtitulo}</div>
-            <div class="categoriaIndex categoria" onclick="DisplayOneProducto(${servicio.id});">${servicio.Categoría}</div>
-            <div class="precioIndex precio" onclick="DisplayOneProducto(${servicio.id});">$${servicio.precio}</div>
-            <button onclick="ListadoCarrito(${servicio.id});">Añadir al Carrito</button>
+            <div class="tituloIndex titulo" onclick="DisplayOneProducto(${item.id});">${item.titulo}</div>
+            <div class="subtituloIndex subtitulo" onclick="DisplayOneProducto(${item.id});">${item.subtitulo}</div>
+            <div class="categoriaIndex categoria" onclick="DisplayOneProducto(${item.id});">${item.Categoría}</div>
+            <div class="precioIndex precio" onclick="DisplayOneProducto(${item.id});">$${item.precio}</div>
+            <button onclick="ListadoCarrito(${item.id}, 'Index');">Añadir al Carrito</button>
         </div>
         `;
-        if(counterServicios < 1){
-            $('#firstItemServicios').html(card)
+        if(counterItem < 1){
+            $(`#firstItem${itemTipe}`).html(card)
         }else {
             carouselItem.className = 'carousel-item';
             carouselItem.innerHTML = card;
-            $('#carouselInnerServicios').append(carouselItem)
+            $(`#carouselInner${itemTipe}`).append(carouselItem)
         }
-            counterServicios ++;
+            counterItem ++;
     }
 }
-
 let totaldeproductos = 0;
 let totalcarrito = 0;
 let Carrito = [];
 let storagevalues = localStorage.Carrito;
-
+let Productos;
 $(async ()=>  {
     const Categorias = await getCategories();
     let ProductosyServicios = await getProducts();
     ProductosyServicios = agregarCategorias(ProductosyServicios, Categorias)
-    let [Productos, Servicios] = SeparateProductosSercicios(ProductosyServicios)
-    DisplayCarouselServicios(Servicios);
-    
+    Productos = ProductosyServicios;
+    let [productos, servicios] = SeparateProductosSercicios(ProductosyServicios)
+    DisplayCarousel(servicios, 'Servicios');
+    DisplayCarousel(productos, 'Productos');
     // DisplayCarouselProductosyServicios();    
     if (storagevalues === null){
         totaldeproductos = 0;
         totalcarrito = 0;
         Carrito = [];
-        
     }else{
         Carrito = JSON.parse(localStorage.ListaCarrito);
     }
